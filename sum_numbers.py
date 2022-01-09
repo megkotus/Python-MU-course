85c72978972236825d837290910d679468dacb07
megkotus <97348644+megkotus@users.noreply.github.com>
2022-01-10 00:08:03 +0300

    Update sum_numbers.py

--- sum_numbers.py
+++ sum_numbers.py
@@ -1,6 +1,14 @@
 import re
 hand = open('mbox.txt')
+tot = list()
+count = 0
+new = 0
 for line in hand :
-	y = re.findall('New Revision: '('[0-9]+)',line))
-	if y !=1 : continue
-	print(y)
+	y = re.findall('^New Revision: ([0-9]+)',line)
+	if len(y) !=1 : continue
+	count = count + 1
+	#extract number from a list
+	for i in y :
+		x = float(i)
+		new = new + x
+print(new/count)
